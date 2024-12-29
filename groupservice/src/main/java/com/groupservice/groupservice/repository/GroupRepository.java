6a8e5dff934f87d36f7e078a95d8cd7e15020f02
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-12-29 19:31:16 +0300

    Delete groupservice directory

--- groupservice/src/main/java/com/groupservice/groupservice/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.groupservice.groupservice.repository;
-
-import com.groupservice.groupservice.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
